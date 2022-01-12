0f46863d0944ac20279daaf97410919be5188ec9
Henry Enamorado <henryenamo@gmail.com>
2022-01-12 17:59:02 -0500

    header finally working, now finishing web hooks for payouts we still missing having payout enable in the account

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const util = require('util')
 var app = express();
 require('dotenv').config();
 var bitauth = require('bitauth');
-console.log(bitauth);
 //change this to (https://bitpay.com) when it is ready for production
 const Environment = "https://test.bitpay.com";
 let signature;
@@ -14,71 +13,26 @@ var crypto = require("crypto");
 let pairingCode;
 app.set("port", process.env.PORT || 3000);
 
-let x = bitauth.generateSin();
-process.env.GENERATEDTOKEN = x.priv;
-console.log(util.inspect(x, false, null, true));
 
-
-
-
-// const getKeysAndSign=(combinedText)=>{
-// //Creating keys
-// const {publicKey,privateKey} = crypto.generateKeyPairSync("rsa",{
-//     //YES ITS NAME IS "modulusLength" :|
-//     modulusLength: 2048,
-//     publicKeyEncoding: {
-//         type: "spki",
-//         format: "der"
-//     },
-//     privateKeyEncoding:{
-//         type: "pkcs8",
-//         format: "der"
-//     }
-// })
-
-// let x ={privateKey:privateKey.toString('hex')}
-// let y = {publicKey:publicKey.toString('hex')}
-// process.env.privateKey =x.privateKey;
-// process.env.publicKey=y.publicKey;
-// console.log(x);
-// /////////////////////////////////////////////////////////////////////////////
-// // Sign the string
-// let string =combinedText;
-// let privateKeyString= x.privateKey;
-// // console.log(privateKeyString);
-// privateKeyString = crypto.createPrivateKey({
-//     key: Buffer.from(privateKeyString,"hex"),
-//     type: "pkcs8",
-//     format: "der"
-// })
-// // console.log(privateKeyString);
-// const sign = crypto.createSign("SHA256")
-// sign.update(string)
-// sign.end()
-// signature= sign.sign(privateKeyString).toString("hex");
-
-// // console.log("777****"+signature);
-// ////////////////////////////////////////////////////
-// }
 
 
 //Webhook to create a invoice
 app.get('/createinvoice', async (req,res)=>{
-    console.log(process.env.BITPAYPUBLICKEY);
-    // getKeysAndSign(`https://test.bitpay.com/invoices{'currency':'USD','price':5,'token':'${process.env.TOKEN}'}`);
+    
     const hookUrl= `${Environment}/invoices`;
-    let dataToSign = hookUrl+`{"currency":"USD","price":5,"token":'${process.env.MERCHANT_TOKEN}'}`;
-    // let signatureGen = bitauth.sign(dataToSign,process.env.MERCHANT_TOKEN);
-    // console.log(process.env.SIN);
+    //this should be content from the body
+    //this body variable have to be separate in order to work
+    let body= {"currency":"USD","price":5,"token":`${process.env.MerchantAPITOKEN}`};
+    let dataToSign = hookUrl+JSON.stringify(body);
     const options = {
-        "headers": { "X-Accept-Version": "2.0.0","Content-Type":"application/json"
-        ,"X-Signature":bitauth.sign(dataToSign,process.env.BITPAYPUBLICKEY)},
+        "headers": { "X-Accept-Version": "2.0.0","Content-Type":"application/json","X-Identity":bitauth.getPublicKeyFromPrivateKey(process.env.privateKey)
+        ,"X-Signature":bitauth.sign(dataToSign,process.env.privateKey).toString('hex')},
         "method": "POST",
         //I MAY NEED JSON.stringify()
-        "body":JSON.stringify( {
+        "body":JSON.stringify({
             "currency":"USD",
             "price":5,
-            "token":`${process.env.MERCHANT_TOKEN}`
+            "token":`${process.env.MerchantAPITOKEN}`
         })
     }
     console.log(options);
@@ -93,6 +47,45 @@ app.get('/createinvoice', async (req,res)=>{
                 token: json
     }));  
 })
+
+app.get('/payoutrecipient', async (req,res)=>{
+   
+    const hookUrl= `${Environment}/payouts`;
+    let body= {"currency":"USD","price":5,"token":`${process.env.MerchantAPITOKEN}`};
+    let dataToSign = hookUrl+JSON.stringify(body);
+    
+    console.log(dataToSign);
+    const options = {
+        "headers": { "X-Accept-Version": "2.0.0","Content-Type":"application/json","X-Identity":bitauth.getPublicKeyFromPrivateKey(process.env.privateKey)
+        ,"X-Signature":bitauth.sign(dataToSign,process.env.privateKey).toString('hex')},
+        "method": "POST",
+        //I MAY NEED JSON.stringify()
+        "body":JSON.stringify({
+            "currency":"USD",
+            "amount":5,
+            "instructions":[{
+                "amount":5,
+                "recipientId":""
+            }],
+            "effectiveDate":"2021-05-24",
+            "token":`${process.env.MerchantAPITOKEN}`
+        })
+    }
+    console.log(options);
+    const response = await Fetch(hookUrl, options)
+    const json = await response.json();
+    console.log(json);
+    // console.log(process.env.AcquieredTOKEN);
+    res.json(JSON.stringify(
+                {
+                status: "success",
+                code: 200,
+                token: json
+    }));  
+})
+
+
+
 //webhook to get API tokens to get in use in invoices creation
 app.get('/gettoken', async (req,res)=>{
     const hookUrl= `${Environment}/tokens`;
@@ -102,7 +95,7 @@ app.get('/gettoken', async (req,res)=>{
         "headers": { "X-Accept-Version": "2.0.0","Content-Type":"application/json"},
         "method": "POST",
         //I MAY NEED JSON.stringify()
-        "json":JSON.stringify( {
+        "body":JSON.stringify( {
             "id":`${process.env.SIN}`,
             "facade":"payroll",
             "label":"test payroll Investacard"
@@ -171,7 +164,8 @@ app.get('/getinvoicescreated', async (req,res)=>{
     }));
     // 
 })
-app.get('/sentcryptoinvitation', async (req,res)=>{
+//this only work the the "payroll" facade
+app.get('/inviterecipient', async (req,res)=>{
     const hookUrl= `${Environment}/recipients`;
     const options = {
         "headers": { "X-Accept-Version": "2.0.0","Content-Type":"application/json"},
@@ -181,7 +175,7 @@ app.get('/sentcryptoinvitation', async (req,res)=>{
             "recipient":[{
                 "email": "henryenamo@gmail.com"
             }],
-            "token":`${process.env.POS}`
+            "token":`${process.env.PoSAPITOKEN}`
         })
     }
     console.log(options);
